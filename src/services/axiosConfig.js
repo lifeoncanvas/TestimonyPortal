ab7ea92caaabf5c206c8e88f8d0eecf3fc3e0268
sharonshelke1 <sharonshelke1@gmail.com>
2026-09-14 11:27:15 +0530

    changes for kingchat fixes

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiBaseUrl = (process.env.REACT_APP_API_URL || "http://148.66.154.48:8083").replace(/\/$/, "");
+const apiBaseUrl = (process.env.REACT_APP_API_URL || "https://148.66.154.48:8083").replace(/\/$/, "");
 
 const api = axios.create({
   baseURL: apiBaseUrl,
